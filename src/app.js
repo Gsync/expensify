c168eb1ca22039ee808123e68070797a221dff60
Khuram Niaz <knshaikh@gmail.com>
2018-01-14 18:15:50 -0700

    edit expense props

--- src/app.js
+++ src/app.js
@@ -1,63 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import "./styles/styles.scss";
 import 'normalize.css/normalize.css';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        this is from my dashboard component..
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        Edit Page
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        this is from my help page
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        this is from my expense page component..
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404! Page not found!
-        <Link to="/">Go to Homepage</Link>
-    </div>
-);
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-        <NavLink to="/edit"activeClassName="is-active">Edit Expense</NavLink>
-        <NavLink to="/help"activeClassName="is-active">Help</NavLink>
-    </header>
-);
-
-const Routes = () => (
-  <BrowserRouter>
-    <div>
-        <Header />
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true} />
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage} />
-        </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(<Routes />, document.getElementById("app"));
+ReactDOM.render(<AppRouter />, document.getElementById("app"));
---
+++ src/components/AddExpense.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddExpensePage = () => (
+  <div>this is from my expense page component..</div>
+);
+
+export default AddExpensePage;
