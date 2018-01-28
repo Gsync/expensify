6a0227738a8072fce92d8281262b92e1d09e3fe3
Khuram Niaz <knshaikh@gmail.com>
2018-01-27 18:40:42 -0700

    Environment variables using dotenv module

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyA93kA68KVUYXU42I000rfoK2-X-ekAfrM",
-    authDomain: "expensify-react-demo.firebaseapp.com",
-    databaseURL: "https://expensify-react-demo.firebaseio.com",
-    projectId: "expensify-react-demo",
-    storageBucket: "expensify-react-demo.appspot.com",
-    messagingSenderId: "120917336997"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 
   firebase.initializeApp(config);
