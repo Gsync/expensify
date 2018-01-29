e1a573e5d1fc7599be0f71a2ca4954fa85be75d0
Khuram Niaz <knshaikh@gmail.com>
2018-01-28 20:10:07 -0700

    Auth reducer to store userid in redux store

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,11 +1,20 @@
 import { firebase, googleAuthProvider } from '../firebase/firebase';
 
+export const login = (uid) => ({
+    type: 'LOGIN',
+    uid
+});
+
 //start the login process and return a function to call firebase related method
 export const startLogin = () => {
     return () => {
         return firebase.auth().signInWithPopup(googleAuthProvider);
     };
 };
+
+export const logout = (uid) => ({
+    type: 'LOGOUT',
+});
 
 export const startLogout = () => {
     return () => {
--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
+import { login, logout } from './actions/auth';
 import getVisibleExpenses from './selectors/expenses';
 import "./styles/styles.scss";
 import 'normalize.css/normalize.css';
@@ -12,16 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
 
 const store = configureStore();
-
-// store.dispatch(addExpense({ description: 'Water bill', amount: 1000, createdAt: -21000 }));
-// store.dispatch(addExpense({ description: 'Gas bill', amount: 200, createdAt: 1100 }));
-// store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }));
-
-// store.dispatch(setTextFilter('water'));
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
 
 const jsx = (
     <Provider store={store}>
@@ -43,6 +33,7 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById("app"));
 //Method to check the login state, whether the user is logged in or not
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
+        store.dispatch(login(user.uid));
         store.dispatch(startSetExpenses()).then(() => {
             renderApp();
             if (history.location.pathname === '/') {
@@ -50,6 +41,7 @@ firebase.auth().onAuthStateChanged((user) => {
             }
         });
     } else {
+        store.dispatch(logout());
         renderApp();
         history.push('/');
     }
