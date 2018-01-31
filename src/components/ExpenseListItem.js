2f3c6b84027ea17c46e89ada4a7e39e74471676f
Khuram Niaz <knshaikh@gmail.com>
2018-01-31 16:51:13 -0700

    Expense list style

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,16 +4,13 @@ import moment from 'moment';
 import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
+    <Link className="list-item" to={`/edit/${id}`}>
     <div>
-        <Link to={`/edit/${id}`}>
-            <h2>{description}</h2>
-        </Link>
-        <p>
-            {numeral(amount/100).format('$0,0.00')}
-            -
-            {moment(createdAt).format('MMMM Do, YYYY')}
-        </p>
+        <h2>{description}</h2>
+        <span>{numeral(amount / 100).format('$0,0.00')}</span>
     </div>
+        <h3>{moment(createdAt).format('MMMM Do, YYYY')}</h3>
+    </Link>
 );
 
 export default ExpenseListItem;
